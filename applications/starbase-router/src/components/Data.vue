469bb9ec50f15c12fbf2481e692e30f3608f68f2
sanrules <sankiry93@gmail.com>
2020-01-16 20:35:13 +0100

    starbase router

---
+++ applications/starbase-router/src/components/Data.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="col-md-12">
+    <Item v-for="(item, index) in items"
+          key="index"
+          :item="item"/>
+  </div>
+</template>
+
+<script>
+  import Item from './Item';
+
+  export default {
+    data() {
+      return {
+        type: this.$route.params.type,
+        items: []
+      }
+    },
+    watch: {
+      '$route': 'fetchItems'
+    },
+    methods: {
+      fetchItems() {
+        this.items = [];
+        let initial_ids = [1, 13, 14];
+
+        for (let i in initial_ids) {
+          let id = initial_ids[i];
+          console.log('id', id);
+
+          fetch(`https://swapi.co/api/${this.type}/${id}`, {method: 'GET'}
+          ).then(response => response.json()).then(json => this.items.push(json))
+        }
+      }
+    },
+    created() {
+      this.fetchItems()
+    },
+    components: {
+      Item
+    }
+  }
+</script>
